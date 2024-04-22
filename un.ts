a98d11e3b81f5c13fee2ac5fc309998a703c97cf
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-04-23 01:56:20 +0500

    Add files via upload

---
+++ un.ts
@@ -0,0 +1,6 @@
+// unary operators
+let a: number=2
+let b: number=3
+console.log(a,++a,a++,a,a++,++a);
+//console.log(a + ++a + a++ + a + a++ + ++a);
+console.log(b,++b,++b,b++,b++,b,++b,++b,b++,b++,++b);
